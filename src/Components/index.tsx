a5de8b1f8de10a78797f4afaf6006947c0dc7552
sac6inarya <sachinkarya333@gmail.com>
2023-05-24 13:18:19 +0530

    add example

--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { ReactForm } from "react-forms-lite";
+import "react-forms-lite/dist/index.css";
 
 function Home() {
-  return <div>Home</div>;
+  const myConfig = [
+    {
+      type: "text",
+      valueKey: "name",
+      fieldProps: {
+        placeholder: "Enter name",
+      },
+    },
+  ];
+  return (
+    <div>
+      <ReactForm
+        config={myConfig}
+        formId={"react-form"}
+        initialValues={{}}
+        onSubmit={(value) => console.log(value)}
+      />
+    </div>
+  );
 }
 
 export default Home;
