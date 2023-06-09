82ad93c6562121e5025df2b564617e3b59c3f826
sac6inarya <sachinkarya333@gmail.com>
2023-06-09 11:19:54 +0530

    test

--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Home() {
-  return <div>Home</div>;
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    // count = count + 1; //does not works because state are immutable , and component do not rerender if we change the state directly
+    setCount(count + 1);
+    console.log(count);
+  };
+  return (
+    <div>
+      {count}
+      <button onClick={handleClick}>Update</button>
+    </div>
+  );
 }
 
 export default Home;
