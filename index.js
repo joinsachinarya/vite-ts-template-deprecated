8cc5cffa10379eb8467edf4f2e695acce1be2afc
sac6inarya <sachinkarya333@gmail.com>
2023-07-28 10:39:19 +0530

    add description with the item

--- index.js
+++ index.js
@@ -1,47 +1,81 @@
-const header = document.querySelector("#header-title");
-const itemList = document.querySelector("#items");
-const form = document.querySelector("#myForm")
-const inputbox = document.querySelector(".form-control")
-// const delBtn = document.querySelector(".del");
-// const edtBtn = document.querySelector(".edt")
-
-let newHeader = document.createElement("p");
-newHeader.className = "newHeader";
-newHeader.textContent = "Hello World"
-header.appendChild(newHeader);
+var form = document.getElementById("addform");
+var itemList = document.getElementById("items");
+var filter = document.getElementById("filter");
 
 
-function addListItem(value) {
-    if (value.trim() === "") {
-        console.error("Please enter some value");
-        return;
-    }
-    else {
-        const newLi = document.createElement("li");
-        newLi.classList.add("item", "card", "p-2");
-        newLi.textContent = value;
+//form submit event
+form.addEventListener("submit", addItem);
+//remove event
+itemList.addEventListener("click", removeItem);
+//filter event 
+filter.addEventListener("keyup", filterItems);
+//edit event
+itemList.addEventListener("click", editItem);
 
-        const deleteBtn = document.createElement("button")
-        deleteBtn.classList.add("btn", "my-2", "del");
-        deleteBtn.textContent = "Delete";
-        newLi.appendChild(deleteBtn);
+//Add item function
+function addItem(e) {
+    e.preventDefault();
+    var newItem = document.getElementById("item").value;
+    var li = document.createElement("li");
+    li.className = "list-group-item";
+    li.appendChild(document.createTextNode(newItem)); //add newItem text node into li
 
-        deleteBtn.addEventListener("click", function () {
-            itemList.removeChild(newLi);
-        })
+    var newDes = document.getElementById("des").value;
+    var i = document.createElement("i");
+    i.className = "d-inline ml-2";
+    i.appendChild(document.createTextNode(newDes));
 
-        const editBtn = document.createElement("button")
-        editBtn.classList.add("btn", "my-2", "edt");
-        editBtn.textContent = "Edit"
-        newLi.appendChild(editBtn);
+    //create delete button
+    var deleteBtn = document.createElement("button");
+    deleteBtn.className = "btn btn-danger btn-sm float-right delete";
+    deleteBtn.appendChild(document.createTextNode("X"));
+    li.appendChild(deleteBtn);
 
-        itemList.appendChild(newLi);
+    //create edit button
+    var editBtn = document.createElement("button");
+    editBtn.className = "btn btn-sm float-right edit mr-2";
+    editBtn.appendChild(document.createTextNode("Edit"));
+    li.appendChild(editBtn);
+
+    itemList.appendChild(li);
+    li.appendChild(i);
+    document.getElementById("item").value = "";
+    document.getElementById("des").value = "";
+}
+
+//Remove item function
+function removeItem(e) {
+    if (e.target.classList.contains("delete")) {
+        if (confirm("Are you sure?")) {
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
     }
 }
 
-form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    let inputValue = document.getElementById("inputbox").value;
-    addListItem(inputValue);
-    inputbox.value = "";
-})
+//filter item function
+function filterItems(e) {
+    var text = e.target.value.toLowerCase();
+    var items = itemList.getElementsByTagName("li");
+    var itemsArray = Array.from(items);
+
+    var desS = itemList.getElementsByTagName("i");
+    var desArray = Array.from(desS);
+
+    itemsArray.forEach(function (it, idx) {
+        var itName = it.firstChild.textContent;
+        var desName = desArray[idx].firstChild.textContent;
+        if (itName.toLocaleLowerCase().indexOf(text) !== -1 || desName.toLocaleLowerCase().indexOf(text) !== -1) {
+            it.style.display = "block"
+        } else {
+            it.style.display = "none"
+        }
+    })
+}
+
+//editItem function
+function editItem(e) {
+    if (e.target.classList.contains("edit")) {
+        console.log("Edit clicked");
+    }
+}
