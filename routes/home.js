79d2ae74ae8bd3ff8230ff69561dfacec404ff85
sac6inarya <sachinkarya333@gmail.com>
2023-08-16 18:16:44 +0530

    stores all message in txt file

--- routes/home.js
+++ routes/home.js
@@ -4,16 +4,18 @@ const bodyParser = require("body-parser");
 
 const router = express.Router();
 router.use(bodyParser.urlencoded({ extended: false }));
+
 router.get("/", (req, res) => {
   let texFileMessage = fs.readFileSync("message.txt", "utf-8");
   res.send(
-    `<form method="POST" action="/message"><p>${texFileMessage}</p><input type="text" name="message" placeholder="Type message"><button type="submit">Send</button></form>`
+    `<form method="POST" action="/message"><input type="text" name="message" placeholder="Type message"><button type="submit">Send</button></form>
+    <div>${texFileMessage}</div>`
   );
 });
 
 router.post("/message", (req, res) => {
   let msg = req.body.message;
-  fs.writeFileSync("message.txt", msg);
+  fs.appendFileSync("message.txt", msg + "\n");
   res.redirect("/");
 });
 module.exports = router;
--- routes/login.js
+++ routes/login.js
@@ -1,21 +1,14 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-
 const router = express.Router();
-router.use(bodyParser.urlencoded({ extended: false }));
-const users = [];
 
 router.get("/login", (req, res) => {
   res.send(
-    `<form method="POST" action="/login"><input type="text" name="username" placeholder="Type username"><button type="submit">Log in</button></form>`
+    `<form method="POST" action="/login" onsubmit="localStorage.setItem('username', document.getElementById('username').value)"><input type="text" name="username" id="username" placeholder="Type username"><button type="submit">Log in</button></form>`
   );
 });
 
 router.post("/login", (req, res) => {
-  let username = req.body.username;
-  users.push(username);
-  console.log(`Logged in as ${username}`);
-  // console.log(users);
+  console.log(`Logged in!`);
   res.redirect("/");
 });
 module.exports = router;
