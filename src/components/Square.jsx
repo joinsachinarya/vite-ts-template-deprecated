c8777a6b58f92937ea8e9660ce48dc1512c30099
sac6inarya <sachinkarya333@gmail.com>
2023-09-16 22:55:41 +0530

    lift the state up

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,25 +1,20 @@
-import { useState } from "react";
-
-const Square = () => {
-  const [value, setValue] = useState("");
-  const [clickCount, setClickCount] = useState(0);
-  const handleClick = () => {
-    setClickCount(clickCount++);
-    console.log(clickCount);
-    // if (clickCount % 2 === 0) {
-    //   setValue("❌");
-    //   setClickCount(() => {
-    //     clickCount = clickCount + 1;
-    //   });
-    // } else {
-    //   setValue("⭕");
-    //   setClickCount(() => {
-    //     clickCount = clickCount + 1;
-    //   });
-    // }
-  };
+const Square = (props) => {
+  const { value, onSquareClick } = props;
+  // const [value, setValue] = useState("");
+  // const [clickCount, setClickCount] = useState(0);
+  // const handleClick = () => {
+  //   // setClickCount(clickCount + 1);
+  //   // console.log(clickCount);
+  //   // if (clickCount % 2 === 0) {
+  //   setValue("❌");
+  //   //   setClickCount(clickCount + 1);
+  //   // } else {
+  //   //   setValue("⭕");
+  //   //   setClickCount(clickCount + 1);
+  //   // }
+  // };
   return (
-    <button className="box" onClick={handleClick}>
+    <button className="box" onClick={onSquareClick}>
       {value}
     </button>
   );
