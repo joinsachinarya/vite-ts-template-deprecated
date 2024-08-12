35dad5a4f06365d37698373a5fed2ce151b87f42
Sachin Arya <sachinkarya333@gmail.com>
2024-08-12 18:04:13 +0530

    khoj: you can't undo redo in chekbox input

--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Home() {
-  return <div>Home</div>;
+  const [text, setText] = useState('Hellooooooooo')
+  const [checked, setChecked] = useState(false);
+  const handleOnChange = (e: any) => {
+    setText(e.target.value)
+  }
+  console.log(checked, text);
+
+  return (
+    <div className="m-2 flex gap-2">
+      <input type="checkbox" name="hi" id="hi" checked={checked} onChange={() => setChecked(!checked)} />
+      <input type="text" onChange={handleOnChange} value={text} />
+    </div>
+  )
 }
 
 export default Home;
